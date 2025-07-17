4ae29fb56b9d43fb263be4175fcf531e0611ec39
amnamine <amineipad98@gmail0com>
2025-07-18 00:00:44 +0100

    h

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const SQLiteStore = require('connect-sqlite3')(session);
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 // Middleware
 app.use(cors());
